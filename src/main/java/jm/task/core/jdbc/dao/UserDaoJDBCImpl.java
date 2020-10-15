7e69c90db03099dc10bd84420c7fbc209fb24a34
GoldDever <69146353+GoldDever@users.noreply.github.com>
2020-10-16 00:13:25 +0600

    Добавлены ролбэки

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
+Connection con;
     public UserDaoJDBCImpl() {
 
     }
@@ -19,12 +19,20 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
        String str ="CREATE TABLE IF NOT EXISTS SuperTable(id INT NOT NULL AUTO_INCREMENT," +
                                    "name VARCHAR(255) NOT NULL, lastName varchar (255) NOT NULL, age TINYINT(10), PRIMARY KEY(id))";
+
         try (Connection connection = Util.getBaseConnaction();
               Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             statement.executeUpdate(str);
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
@@ -32,10 +40,17 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         String str ="DROP TABLE IF EXISTS SuperTable";
         try (Connection connection = Util.getBaseConnaction(); Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             statement.executeUpdate(str);
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
@@ -43,10 +58,17 @@ public class UserDaoJDBCImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         String str = "INSERT INTO SuperTable(name, lastName, age) VALUES('" + name + "','" + lastName + "'," + age + ")";
         try (Connection connection = Util.getBaseConnaction(); Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             statement.executeUpdate(str);
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
         System.out.printf("User с именем – %s добавлен в базу данных" + "\n", name);
@@ -55,10 +77,17 @@ public class UserDaoJDBCImpl implements UserDao {
     public void removeUserById(long id) {
         String str = "DELETE FROM SuperTable WHERE id = " + id;
         try (Connection connection = Util.getBaseConnaction(); Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             statement.executeUpdate(str);
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
@@ -67,6 +96,7 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> list = new ArrayList<>();
         ResultSet resultSet = null;
         try (Connection connection = Util.getBaseConnaction(); Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             resultSet = statement.executeQuery("select * from SuperTable");
             while (resultSet.next()) {
@@ -77,21 +107,33 @@ public class UserDaoJDBCImpl implements UserDao {
                 list.add(user);
             }
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
-
         return list;
     }
 
     public void cleanUsersTable() {
         String str = "TRUNCATE TABLE SuperTable";
         try (Connection connection = Util.getBaseConnaction(); Statement statement = connection.createStatement()){
+            con = connection;
             connection.setAutoCommit(false);
             statement.executeUpdate(str);
             connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException throwables) {
-
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            throwables.printStackTrace();
         }
     }
 }
